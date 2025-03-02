d9825f284b05f33989346993f5d3bde35e58c856
wiras-4 <abangdrzomboss@gmail.com>
2025-03-02 16:41:13 +0700

    first commit

---
+++ assets/js/biodata.js
@@ -0,0 +1,31 @@
+const biodata = {
+    avatar: "./assets/images/avatar-1.png",
+    name: "Abdullah Fahd",
+    job: "Student",
+    contactList: [
+        {
+            icon: '<ion-icon name="mail-outline"></ion-icon>',
+            title: "Email",
+            info: "a.fahd.assajjad@gmail.com",
+            link: "mailto:a.fahd.assajjad@gmail.com"
+        },
+        {
+            icon: '<ion-icon name="call-outline"></ion-icon>',
+            title: "Phone",
+            info: "+62 813-8243-5048",
+            link: "https://wa/me/+62813-8243-5048"
+        },
+        {
+            icon: '<ion-icon name="calendar-outline"></ion-icon>',
+            title: "Birthday",
+            info: "18 February 2007",
+            link: "#"
+        },
+        {
+            icon: '<ion-icon name="location-outline"></ion-icon>',
+            title: "Location",
+            info: "Sawangan, Depok",
+            link: "#"
+        }
+    ]
+}
---
+++ assets/js/script.js
@@ -0,0 +1,200 @@
+'use strict';
+
+
+
+// element toggle function
+const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
+
+
+
+// sidebar variables
+const sidebar = document.querySelector("[data-sidebar]");
+const sidebarBtn = document.querySelector("[data-sidebar-btn]");
+
+// sidebar toggle functionality for mobile
+sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
+
+
+
+// testimonials variables
+const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
+const modalContainer = document.querySelector("[data-modal-container]");
+const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
+const overlay = document.querySelector("[data-overlay]");
+
+// modal variable
+const modalImg = document.querySelector("[data-modal-img]");
+const modalTitle = document.querySelector("[data-modal-title]");
+const modalText = document.querySelector("[data-modal-text]");
+
+// modal toggle function
+const testimonialsModalFunc = function () {
+  modalContainer.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
+
+// add click event to all modal items
+for (let i = 0; i < testimonialsItem.length; i++) {
+
+  testimonialsItem[i].addEventListener("click", function () {
+
+    modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
+    modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
+    modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
+    modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
+
+    testimonialsModalFunc();
+
+  });
+
+}
+
+// add click event to modal close button
+modalCloseBtn.addEventListener("click", testimonialsModalFunc);
+overlay.addEventListener("click", testimonialsModalFunc);
+
+
+
+// custom select variables
+const select = document.querySelector("[data-select]");
+const selectItems = document.querySelectorAll("[data-select-item]");
+const selectValue = document.querySelector("[data-selecct-value]");
+const filterBtn = document.querySelectorAll("[data-filter-btn]");
+
+select.addEventListener("click", function () { elementToggleFunc(this); });
+
+// add event in all select items
+for (let i = 0; i < selectItems.length; i++) {
+  selectItems[i].addEventListener("click", function () {
+
+    let selectedValue = this.innerText.toLowerCase();
+    selectValue.innerText = this.innerText;
+    elementToggleFunc(select);
+    filterFunc(selectedValue);
+
+  });
+}
+
+// filter variables
+const filterItems = document.querySelectorAll("[data-filter-item]");
+
+const filterFunc = function (selectedValue) {
+
+  for (let i = 0; i < filterItems.length; i++) {
+
+    if (selectedValue === "all") {
+      filterItems[i].classList.add("active");
+    } else if (selectedValue === filterItems[i].dataset.category) {
+      filterItems[i].classList.add("active");
+    } else {
+      filterItems[i].classList.remove("active");
+    }
+
+  }
+
+}
+
+// add event in all filter button items for large screen
+let lastClickedBtn = filterBtn[0];
+
+for (let i = 0; i < filterBtn.length; i++) {
+
+  filterBtn[i].addEventListener("click", function () {
+
+    let selectedValue = this.innerText.toLowerCase();
+    selectValue.innerText = this.innerText;
+    filterFunc(selectedValue);
+
+    lastClickedBtn.classList.remove("active");
+    this.classList.add("active");
+    lastClickedBtn = this;
+
+  });
+
+}
+
+
+
+// contact form variables
+const form = document.querySelector("[data-form]");
+const formInputs = document.querySelectorAll("[data-form-input]");
+const formBtn = document.querySelector("[data-form-btn]");
+
+// add event to all form input field
+for (let i = 0; i < formInputs.length; i++) {
+  formInputs[i].addEventListener("input", function () {
+
+    // check form validation
+    if (form.checkValidity()) {
+      formBtn.removeAttribute("disabled");
+    } else {
+      formBtn.setAttribute("disabled", "");
+    }
+
+  });
+}
+
+
+
+// page navigation variables
+const navigationLinks = document.querySelectorAll("[data-nav-link]");
+const pages = document.querySelectorAll("[data-page]");
+
+// add event to all nav link
+for (let i = 0; i < navigationLinks.length; i++) {
+  navigationLinks[i].addEventListener("click", function () {
+
+    for (let i = 0; i < pages.length; i++) {
+      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
+        pages[i].classList.add("active");
+        navigationLinks[i].classList.add("active");
+        window.scrollTo(0, 0);
+      } else {
+        pages[i].classList.remove("active");
+        navigationLinks[i].classList.remove("active");
+      }
+    }
+
+  });
+}
+document.querySelector('.avatar-box').innerHTML = `
+  <img src="${biodata.avatar}" alt="${biodata.name}" width="80">
+`
+
+document.querySelector('.info-content').innerHTML = `
+  <h1 class="name" title="${biodata.name}">${biodata.name}</h1>
+  <p class="title">${biodata.job}</p>
+`
+
+let contactListHtml = ``
+biodata.contactList.forEach(contact => {
+  contactListHtml += `
+  <li class="contact-item">
+    <div class="icon-box">
+      ${contact.icon}
+    </div>
+
+    <div class="contact-info">
+      <p class="contact-title">${contact.title}</p>
+
+      <a href="${contact.link}" class="contact-link">${contact.info}</a>
+    </div>
+
+  </li>
+  `
+})
+
+document.querySelector('.contacts-list').innerHTML = contactListHtml
+document.querySelector('.about-text').innerHTML = `
+  <p>
+    I'm a senior high school student with a big dream of becoming a naval engineer. 
+    I'm fascinated by ships, structures, and the science behind building things that move and withstand the forces of nature. 
+    This passion drives me to learn more about engineering and design, hoping to one day contribute to the world of naval architecture.
+  </p>
+
+  <p>
+    At the same time, I'm exploring my creative side as a beginner in UI/UX and graphic design. 
+    I love creating designs that are both functional and visually appealing, bringing ideas to life through thoughtful layouts and user-friendly experiences. 
+    For me, design is a way to tell stories and solve problems, and I'm excited to keep growing in this field.
+  </p>
+`
